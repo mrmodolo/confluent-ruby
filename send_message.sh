233c9654b378ff6db0af0bf155daef91c965fa78
mrmodolo <marcelo.rmodolo@gmail.com>
2023-05-07 11:02:34 -0300

    Testanto o runtime (podman ou docker)

--- send_message.sh
+++ send_message.sh
@@ -1,11 +1,47 @@
 #!/bin/bash
 
-source ~/.confluent/karafka.config
+CONFIG_FILE="${HOME}/.confluent/karafka.config"
 
-podman run -it --rm \
+run_container() {
+${RUNTIME} run -it --rm \
   -e KAFKA_CLIENT_ID=$KAFKA_CLIENT_ID \
   -e KAFKA_GROUP_ID_PREFIX=$KAFKA_GROUP_ID_PREFIX \
   -e KAFKA_BOOTSTRAP_SERVERS=$KAFKA_BOOTSTRAP_SERVERS \
   -e KAFKA_SASL_USERNAME=$KAFKA_SASL_USERNAME \
   -e KAFKA_SASL_PASSWORD=$KAFKA_SASL_PASSWORD \
   --name send-ruby-karafka-confluent ruby-karafka-confluent bash -c "echo \"Karafka.producer.produce_sync(topic: 'partnerNotifications_baas_v0', payload: { 'ping' => 'pong' }.to_json)\" | bundle exec karafka c"
+}
+
+if [ ! -f ${CONFIG_FILE} ]
+then
+  echo "O arquivo de configuração em ${CONFIG_FILE} não foi encontrado."
+  echo "As variáveis abaixo necessitam ser definidas:"
+  echo "  KAFKA_CLIENT_ID"
+  echo "  KAFKA_GROUP_ID_PREFIX"
+  echo "  KAFKA_BOOTSTRAP_SERVERS"
+  echo "  KAFKA_SASL_USERNAME"
+  echo "  KAFKA_SASL_PASSWORD"
+  exit 1
+fi
+
+source ${CONFIG_FILE}
+
+# Eu prefiro o podman
+RUNTIME=$(which podman)
+
+if [[ ${RUNTIME} && $(${RUNTIME} --version) ]]
+then
+  run_container
+else
+  # Não temos o podman vamos tentar com o docker
+    RUNTIME=$(which docker)
+    if [[ ${RUNTIME} && $(${RUNTIME} --version) ]]
+    then
+      run_container
+    else
+      echo "É necessário que o podman ou docker estejam instalados"
+      exit 1
+    fi
+fi
+
+exit 0
