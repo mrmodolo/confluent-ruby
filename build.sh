830ab458c71ff7f079c0d1e04966077a126535af
mrmodolo <marcelo.rmodolo@gmail.com>
2023-05-07 11:20:25 -0300

    Testa para podman ou docker

--- build.sh
+++ build.sh
@@ -1,3 +1,24 @@
 #!/usr/bin/env bash
 
-podman build -t ruby-karafka-confluent .
+run_container() {
+  ${RUNTIME} build -t ruby-karafka-confluent .
+}
+
+# Eu prefiro o podman
+RUNTIME=$(which podman)
+
+if [[ ${RUNTIME} && $(${RUNTIME} --version) ]]
+then
+  run_container
+else
+  # Não temos o podman vamos tentar com o docker
+    RUNTIME=$(which docker)
+    if [[ ${RUNTIME} && $(${RUNTIME} --version) ]]
+    then
+      run_container
+    else
+      echo "É necessário que o podman ou docker estejam instalados"
+      exit 1
+    fi
+fi
+
